72ebfe85e4ff49d6ba27c122095e3923dbe6f5a3
Luc <80841842+zLuc-us@users.noreply.github.com>
2022-10-08 03:16:18 -0300

    Create userUpdate.ts

---
+++ userUpdate.ts
@@ -0,0 +1,17 @@
+    import { TextBasedChannel, EmbedBuilder, Colors } from "discord.js";
+
+    client.on("userUpdate", async (oldMember, newMember) => {
+      const channel = <TextBasedChannel>(
+        await client.channels.fetch("") //id do canal
+      );
+      const NewEmbed = new EmbedBuilder()
+        .setTitle(`Novo avatar`)
+        .setImage(newMember.avatarURL({ size: 2048 }))
+        .setColor(Colors.DarkAqua);
+
+      const oldEmbed = new EmbedBuilder()
+        .setTitle(`Avatar antigo`)
+        .setImage(oldMember.avatarURL({ size: 2048 }))
+        .setColor(Colors.Aqua);
+      channel!.send({ embeds: [NewEmbed, oldEmbed] });
+    });
